752287e00e0715c914b62171bf7e7487e2eeefda
Austin Albert <aalbert@tipconsulting.io>
2021-02-08 12:58:56 -0700

    Add ESP32 touchpad enums

---
+++ nanoFramework.Hardware.Esp32/CapacitiveTouch/TouchLowVolt.cs
@@ -0,0 +1,51 @@
+﻿//
+// Copyright (c) 2018 The nanoFramework project contributors
+// See LICENSE file in the project root for full license information.
+//
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace nanoFramework.Hardware.Esp32.TouchPad
+{
+    /// <summary>
+    /// Touch sensor low reference voltage
+    /// </summary>
+    /// <remarks>
+    /// Corresponds to touch_low_volt_t
+    /// https://docs.espressif.com/projects/esp-idf/en/release-v4.2/esp32s2/api-reference/peripherals/touch_pad.html#_CPPv416touch_low_volt_t
+    /// </remarks>
+    public enum TouchLowVolt
+    {
+        /// <summary>
+        /// Touch sensor low reference voltage, no change
+        /// </summary>
+        Keep = -1,
+
+        /// <summary>
+        /// Touch sensor low reference voltage, 0.5V
+        /// </summary>
+        L0V5 = 0,
+
+        /// <summary>
+        /// Touch sensor low reference voltage, 0.6V
+        /// </summary>
+        L0V6,
+
+        /// <summary>
+        /// Touch sensor low reference voltage, 0.7V
+        /// </summary>
+        L0V7,
+
+        /// <summary>
+        /// Touch sensor low reference voltage, 0.8V
+        /// </summary>
+        L0V8,
+
+        /// <summary>
+        /// No source documentation available
+        /// </summary>
+        Max
+    }
+}
---
+++ nanoFramework.Hardware.Esp32/CapacitiveTouch/TouchVoltAtten.cs
@@ -0,0 +1,51 @@
+﻿//
+// Copyright (c) 2018 The nanoFramework project contributors
+// See LICENSE file in the project root for full license information.
+//
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace nanoFramework.Hardware.Esp32.TouchPad
+{
+	/// <summary>
+	/// Touch sensor high reference voltage attenuation
+	/// </summary>
+	/// <remarks>
+	/// Corresponds to touch_volt_atten_t
+	/// https://docs.espressif.com/projects/esp-idf/en/release-v4.2/esp32s2/api-reference/peripherals/touch_pad.html#_CPPv418touch_volt_atten_t
+	/// </remarks>
+	public enum TouchVoltAtten
+	{
+		/// <summary>
+		/// Touch sensor high reference voltage attenuation, no change
+		/// </summary>
+		Keep = -1,
+
+		/// <summary>
+		/// Touch sensor high reference voltage attenuation, 1.5V attenuation
+		/// </summary>
+		A1V5 = 0,
+
+		/// <summary>
+		/// Touch sensor high reference voltage attenuation, 1V attenuation
+		/// </summary>
+		A1V,
+
+		/// <summary>
+		/// Touch sensor high reference voltage attenuation, 0.5V attenuation
+		/// </summary>
+		A0V5,
+
+		/// <summary>
+		/// Touch sensor high reference voltage attenuation, 0V attenuation
+		/// </summary>
+		A0V,
+
+		/// <summary>
+		/// No source documentation available
+		/// </summary>
+		Max
+	}
+}

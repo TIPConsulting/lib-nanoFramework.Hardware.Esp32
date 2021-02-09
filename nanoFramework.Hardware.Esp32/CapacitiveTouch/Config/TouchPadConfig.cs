5806886fccbfca32875ef9163eadc568cf42f3c1
Austin Albert <aalbert@tipconsulting.io>
2021-02-08 19:09:38 -0700

    Add TouchPad config types

---
+++ nanoFramework.Hardware.Esp32/CapacitiveTouch/Config/TouchPadConfig.cs
@@ -0,0 +1,28 @@
+﻿//
+// Copyright (c) 2021 The nanoFramework project contributors
+// See LICENSE file in the project root for full license information.
+//
+
+namespace nanoFramework.Hardware.Esp32.TouchPad
+{
+	/// <summary>
+	/// Configuration parameters for TouchPad interrupt based class
+	/// </summary>
+	public class TouchPadConfig : TouchPadConfigBase
+	{
+		/// <summary>
+		/// The threshold to trigger interrupt when the pad is touched.
+		/// Sensor value must be greater than this value to trigger an interrupt
+		/// </summary>
+		public float InterruptThresholdValue { get; set; }
+
+		/// <summary>
+		/// Default constructor
+		/// </summary>
+		public TouchPadConfig() : base()
+		{
+			// By default, use 2/3 of read value as the threshold to trigger interrupt when the pad is touched.
+			InterruptThresholdValue = (float)2 / 3;
+		}
+	}
+}
---
+++ nanoFramework.Hardware.Esp32/CapacitiveTouch/Config/TouchPadReaderConfig.cs
@@ -0,0 +1,25 @@
+﻿//
+// Copyright (c) 2021 The nanoFramework project contributors
+// See LICENSE file in the project root for full license information.
+//
+
+namespace nanoFramework.Hardware.Esp32.TouchPad
+{
+    /// <summary>
+    /// Configuration parameters for TouchPadReader poll based class
+    /// </summary>
+    public class TouchPadReaderConfig : TouchPadConfigBase
+    {
+        /// <summary>
+        /// Set preferred sensor read mode - raw or filtered
+        /// </summary>
+        public TouchPadReadMode ReadMode { get; set; } = TouchPadReadMode.Filtered;
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public TouchPadReaderConfig() : base()
+        {
+        }
+    }
+}
